73fb833d0ece88cfdd0a5193b35bdfce931ffad9
paul <site@webpaul.ru>
2019-04-21 21:11:37 +0600

    hw4

--- src/contexts/Auth/Auth.js
+++ src/contexts/Auth/Auth.js
@@ -1,11 +1,47 @@
 import React, { PureComponent } from 'react';
 
 const { Provider, Consumer: AuthConsumer } = React.createContext('');
-
+const data = {
+  email: 'stu@dent.com',
+  password: '123'
+};
 class AuthProvider extends PureComponent {
+  state = {
+    email: '',
+    authorizeError: '',
+    isAuthorized: false,
+    errorMessage: 'Email или пароль введён не верно'
+  };
+
+  authorize = (email, password) => {
+    const { errorMessage } = this.state;
+    const isEmailValid = data.email === email;
+    const isPasswordValid = data.password === password;
+
+    if (isEmailValid && isPasswordValid) {
+      this.setState({
+        email,
+        authorizeError: '',
+        isAuthorized: true
+      });
+    } else {
+      this.setState({ authorizeError: errorMessage });
+    }
+  };
+
+  logout = () => this.setState({ isAuthorized: false });
+
+  getProviderValue = () => {
+    return {
+      ...this.state,
+      authorize: this.authorize,
+      logout: this.logout
+    };
+  };
+
   render() {
     const { children } = this.props;
-    return <Provider>{children}</Provider>;
+    return <Provider value={this.getProviderValue()}>{children}</Provider>;
   }
 }
 
